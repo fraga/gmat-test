eaab27738a28b47dc708f8d1bfc1f69a472ce2d1
Felipe Seixas <seixasfelipe@gmail.com>
2014-08-18 22:03:15 -0400

    controller scope, choice model?

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 (function() {
 	var app = angular.module('gmatApp', []);
 	
-	app.controller('QuestionController', function(){
-		this.question = questions[0];
-		this.choice = '';
+	app.controller('QuestionController', ['$scope', function($scope){
 		
-	});
-	
-	var questions = [{
+		var questions = [{
 			id:1,
 			type: "Critical Reasoning",
 			description: "Comet A is seen near the Earth every 12 years while comet B every 20 years. If both comets were observed in 1979, for how many years do we have to wait to see the two comets together again? (Assume now is 2004)",
@@ -34,7 +30,13 @@
 				}
 			],
 			correct: "e"
-	}];
-	
+		}];
+
+
+
+		$scope.question = questions[0];
+		$scope.choice = 'aaaaaaa';
+		
+	}]);	
 
 })();
